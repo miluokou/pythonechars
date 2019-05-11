18703c6ea68441a01e3b18a654b820c83236f315
zhangyongshun <348393887@qq.com>
2019-05-11 08:14:10 +0800

    zaoshang update

--- venv/Scripts/demo.py
+++ venv/Scripts/demo.py
@@ -1,27 +1,49 @@
 from flask import Flask,render_template,url_for
 import pymssql
 from sql import MSSQL
-
+from pymongo import MongoClient
+import collections.abc
+# //pip install pymongo
 
 app = Flask(__name__)
 
 
 ms = MSSQL(host="localhost", user="sa", pwd="root", db="xinhai")
+client = MongoClient('localhost', 27017)
+url = 'http://example.webscraping.com/view/united-kingdom-239'
+html = 'f'
+db = client.cache
+resListValue = ms.ExecQuery("SELECT score_raw,score_t from v_result where score_raw is not null and score_t is not null")
+db.resList.insert({'key': 'resListKey', 'resListValue': resListValue})
+ressss = db.webpage.find_one({'key': ''})
 
-@app.route('/demo')
-def my_echart():
+# try:
+#     if 'resList' in db:
+#         resListRes = db.resList
+#     # else:
+#     #     db.resList = ms.ExecQuery("SELECT COUNT(1) from v_result;")
+#     #     resListRes = db.resList
+# except :
+#     db.resList = ms.ExecQuery("SELECT COUNT(1) from v_result;")
+#     resListRes = db.resList
+#     pass
+
+
+# @app.route('/demo')
+# def my_echart():
     # resList = ms.ExecQuery("SELECT top(2) * FROM v_result")
-    resList = ms.ExecQuery("SELECT COUNT(1) from v_result;")
-    return render_template('my_template.html',resList =resList)
+    # resList = ms.ExecQuery("SELECT COUNT(1) from v_result;")
+    # return render_template('my_template.html',resList =resList)
 @app.route('/')
 def index():
 
-    return render_template('index.html')
-@app.route('/hello')
-def hello():
-    resList = ms.ExecQuery("SELECT COUNT(1) from v_result;")
+
+    return render_template('hello.html',db=ressss[html])
+
+# @app.route('/hello')
+# def hello():
     # return json.dumps(resList, ensure_ascii=False)
-    return render_template('hello.html',resList=resList)
+    # return render_template('hello.html',resList=resListRes)
 
 if __name__ == "__main__":
 
