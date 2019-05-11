b61d7b1f9f68d88993a3df9459f72155cdbb412f
zhangyongshun <348393887@qq.com>
2019-05-12 00:25:48 +0800

    MongoDB function

--- venv/Scripts/demo.py
+++ venv/Scripts/demo.py
@@ -1,21 +1,67 @@
 from flask import Flask,render_template,url_for
 import pymssql
-from sql import MSSQL
 from pymongo import MongoClient
 import collections.abc
 # //pip install pymongo
-
+from sql import MSSQL
 app = Flask(__name__)
 
 
 ms = MSSQL(host="localhost", user="sa", pwd="root", db="xinhai")
+# resList = ms.ExecQuery("SELECT * FROM aat1")
+
+
+# # print(resListValue)
 client = MongoClient('localhost', 27017)
-url = 'http://example.webscraping.com/view/united-kingdom-239'
-html = 'f'
 db = client.cache
-resListValue = ms.ExecQuery("SELECT score_raw,score_t from v_result where score_raw is not null and score_t is not null")
-db.resList.insert({'key': 'resListKey', 'resListValue': resListValue})
-ressss = db.webpage.find_one({'key': ''})
+
+def MongoCache(Query,sql):
+    QueryKey = Query+'Key'
+    QueryValue = Query+'Value'
+    res = db[Query].find_one({QueryKey: 'resultTableQueryResult'})
+    try:
+        if QueryValue in res :
+            resExist = res[QueryValue]
+        else:
+            # resultTableQueryResult = ms.ExecQuery("SELECT studentid,score_raw,score_t from result WHERE score_raw is not null")
+            resExist = ms.ExecQuery(sql)
+            db[Query].insert({QueryKey: 'resultTableQueryResult', QueryValue: resExist})
+    except:
+        resExist = ms.ExecQuery(sql)
+        db[Query].insert({QueryKey: 'resultTableQueryResult', QueryValue: resExist})
+        pass
+    return resExist
+resultSql = 'SELECT studentid,score_raw,score_t from result WHERE score_raw is not null'
+resultExistRes = MongoCache('result',resultSql)
+
+    # try:
+    #     if 'QueryValue' in res :
+    #         resExist = res['QueryValue']
+    #     else:
+    #         resultTableQueryResult = ms.ExecQuery("SELECT studentid,score_raw,score_t from result WHERE score_raw is not null")
+    #         resExist =resultTableQueryResult
+    #         db.MssQuery.insert({'QueryKey': 'resultTableQueryResult', 'QueryValue': resultTableQueryResult})
+    # except:
+    #     resultTableQueryResult = ms.ExecQuery("SELECT studentid,score_raw,score_t from result WHERE score_raw is not null")
+    #     resExist = resultTableQueryResult
+    #     db.MssQuery.insert({'QueryKey': 'resultTableQueryResult', 'QueryValue': resultTableQueryResult})
+    #
+
+
+
+# MongoRes = db.col.find()
+# MongoRes = db.MssQuery.find_one({'key': 'resultTableQueryResult'})
+# if "QueryValue" in MongoRes:
+#     reflactData = db.MssQuery.
+# db.resList.insert({'QueryKey': 'resultTableQueryResult', 'QueryValue': resultTableQueryResult})
+# if 'resultTableQueryResult' in ressss:
+
+# db.resList.insert({'key': 'resultTableSueryResult', 'value': resultTableSueryResult})
+# url = 'http://example.webscraping.com/view/united-kingdom-239'
+# html = 'f'
+# db = client.cache
+# db.resList.insert({'key': 'resListKey', 'resListValue': resListValue})
+# ressss = db.webpage.find_one({'key': ''})
 
 # try:
 #     if 'resList' in db:
@@ -37,8 +83,7 @@ ressss = db.webpage.find_one({'key': ''})
 @app.route('/')
 def index():
 
-
-    return render_template('hello.html',db=ressss[html])
+    return render_template('hello.html',resListValue = resultExistRes)
 
 # @app.route('/hello')
 # def hello():
